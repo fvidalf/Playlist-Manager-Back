5d60837e0f29b84fe3322b5a8b2ab9247c712ac1
fvidalf <fvidalf@uc.cl>
2024-12-15 19:11:29 -0300

    refactor(playlists): Change Spotify API endpoint for playlists,  making getting user id redundant. Remove fetch for user id

--- src/routes/playlistRoutes.ts
+++ src/routes/playlistRoutes.ts
@@ -10,23 +10,16 @@ playlistRouter.get("/playlists", async (ctx) => {
     // Given a user's access token, fetch their playlists
     // Needs to login first to get the access token (client-stored)
     const accessToken = ctx.query.access_token;
-    const limit = ctx.query.limit || 20;
-    const offset = ctx.query.offset || 0;
+    const limit = ctx.query.limit || "20";
+    const offset = ctx.query.offset || "0";
 
     try {
-        // https://api.spotify.com/v1/users/{user_id}/playlists this is the endpoint to get the playlists
-        // We need to get the user_id first
-        const userResponse = await axios.get(`${SPOTIFY_URL}/me`, {
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Authorization': `Bearer ${accessToken}`
-            }
+        // https://api.spotify.com/v1/me/playlists this is the endpoint to get the playlists
+        const paginationParams = new URLSearchParams({
+            limit: limit,
+            offset: offset
         });
-
-        const userId = userResponse.data.id;
-
-        // Get the playlists
-        const playlists = await axios.get(`${SPOTIFY_URL}/users/${userId}/playlists?limit=${limit}&offset=${offset}`, {
+        const playlists = await axios.get(`${SPOTIFY_URL}/me/playlists?${paginationParams.toString()}`, {
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded',
                 'Authorization': `Bearer ${accessToken}`
