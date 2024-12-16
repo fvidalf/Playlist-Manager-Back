657d108efc7efe82f26f8d2a09dffa3da2b88a61
fvidalf <fvidalf@uc.cl>
2024-12-16 20:40:48 -0300

    refactor(token-handling): Refactor access token to be stored as a cookie. Add endpoint for clients to check whether access_token has been set as cookie

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import Koa from "koa";
 import logger from "koa-logger";
+import cookie from "koa-cookie";
 import { router } from "./routes/routes";
 import cors from "@koa/cors";
 
 const app = new Koa();
 
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
+app.use(cookie());
 app.use(logger());
 app.use(router.routes());
 app.use(router.allowedMethods());
